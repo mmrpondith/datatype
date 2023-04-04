6c2fb61832a985d5dd6ff54d56e8b8471a2468cc
“mmrpondith” <itibdd@gmail.com>
2023-04-04 16:20:29 +0600

    class3.py

---
+++ class3.py
@@ -0,0 +1,48 @@
+x = "Hello World"
+print(x)
+print(type(x))
+x1 = 20
+print(x1)
+print(type(x1))
+x2 = 20.5
+print(x2)
+print(type(x2))
+
+x3 = 6J
+print(x3)
+print(type(x3))
+
+x4 = ["apple","banana","cherry"]
+print(x4)
+print(type(x4))
+
+x5 = ("apple", "banana", "cherry")
+print(x5)
+print(type(x5))
+
+x6 = range(1,6)
+print(x6)
+print(type(x6)) 
+
+x7 = {"name" : "John", "father's Name": "Tailor",  "age" : 36}
+print(x7)
+print(type(x7)) 
+
+x8 = {"apple", "banana", "cherry"}
+print(x8)
+print(type(x8)) 
+
+x = True
+print(x)
+print(type(x)) 
+
+
+
+
+
+
+
+
+
+
+
